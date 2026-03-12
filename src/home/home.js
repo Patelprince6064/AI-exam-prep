b82c56c0f29813698fa031f4419d0ddd4b50593b
Prince Patel <pp50646464@gmail.com>
2026-03-12 17:06:00 +0530

    Add logout function 6

--- src/home/home.js
+++ src/home/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./home.css";
 import logo from "../assets/logo.png";
-
+<img src={logo} alt="logo" /> 
 
 const Home = () => {
   return (
